9164143079b4f0e851460d5b172dac99236c1995
juanmr8 <moraromerojuan8@gmail.com>
2025-10-12 11:03:45 +0200

    removed unnecessary files

--- app/timeline-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import TimelineAnimationOnly from '@/components/timeline-animation-only';
-
-export default function TimelineDemoPage() {
-  return (
-    <main className='min-h-screen'>
-      <TimelineAnimationOnly />
-    </main>
-  );
-}
